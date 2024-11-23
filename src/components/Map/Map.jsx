8786c51cc52b10f1daf794b4aea76a17f90d5bd5
parisa-adli <parisa.adli1996@gmail.com>
2024-11-23 07:50:29 +0330

    create context for hotels-provider hotels

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,8 @@
+import { useHotels } from "../context/HotelsProvider";
+
+function Map() {
+  const { isLoading, hotels } = useHotels();
+  return <div className="mapContainer">map</div>;
+}
+
+export default Map;
